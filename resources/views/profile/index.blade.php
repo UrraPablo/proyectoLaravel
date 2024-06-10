b0e3ba18a609fabfca65fd38ca1c0111e28f1c48
Alexis Casimiro <exequielcasimiro@gmail.com>
2024-06-10 15:43:10 -0300

    arreglando cosas visuales

--- resources/views/profile/index.blade.php
+++ resources/views/profile/index.blade.php
@@ -15,43 +15,50 @@
                 <div class="card-body">
                     <h6 class="card-title">Nombre: {{$user->name}}</h6>
                     <h6 class="card-text">Email: {{$user->email}}</h6>
-                    <a href="#" class="btn btnlogin">Editar Perfil</a>
+                    <a href="{{ route('profile.edit') }}" class="btn btnlogin">Editar Perfil</a>
                 </div>
             </div>
         </div>
         <div class="col-8">
             <div class="d-flex justify-content-between align-items-center mb-2">
-                <h3>Vibes de {{$user->name}}</h3>
+                <h3 class="card-header card fs-5 headerLogin">Vibes de {{$user->name}}</h3>
                 <a href="{{ route('post.create') }}" class="btn btnlogin">Nueva Vibe</a>
             </div>
             @if($posts->isEmpty())
                 <h5 class="mb-2 text-danger">No hay post disponibles</h5>
             @else
                 @foreach ($posts as $post)
-                <a href="{{ route('category.post.show', ['category' => $post->category->id, 'post' => $post->id]) }}" class="card-home">
-                    <div class="card card-post card-home mb-3">
-                        <div class="card-header card-post-header">
-                            <!-- Fecha de creación -->
-                            <p class="card-date">{{ $post->created_at }}</p>
-                            <span class="card-category position-absolute top-0 end-0 p-1 m-1 fw-bold text-white">{{$post->category->name}}</span>
-                        </div>
-                        <div class="card-body card-post-body">
-                            <!-- Título del post -->
-                            <h5 class="card-title card-post-title">{{ $post->title }}</h5>
-                            <!-- Imagen del post -->
-                            @if($post->imagen)
-                            <img src="{{ asset('storage/' . $post->imagen) }}" class="img-fluid mb-3" style="max-height: 150px; object-fit: cover;" alt="Imagen del post">
-                            @endif
-                            <!-- Contenido del post -->
-                            <p class="card-text card-post-text">{{ $post->content }}</p>
-                            <!-- Botones de Editar y Eliminar -->
-                            <div class="d-flex justify-content-end mt-3">
-                                <a href="{{ route('post.edit', $post->id) }}" class="btn btn-warning btn-sm me-2">Editar</a>
-                                <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal" data-post-id="{{ $post->id }}">Eliminar</button>
+                <!-- Contenedor para los posts -->
+                <div class="col-md-8">
+                    <div class="shadow-lg">
+                        <div class="card-body">
+                        <a href="{{ route('category.post.show', ['category' => $post->category->id, 'post' => $post->id]) }}" class="card-home">
+                            <div class="card card-post card-home mb-3">
+                                <div class="card-header card-post-header">
+                                    <!-- Fecha de creación -->
+                                    <p class="card-date">{{ $post->created_at }}</p>
+                                    <span class="card-category position-absolute top-0 end-0 p-1 m-1 fw-bold text-white">{{$post->category->name}}</span>
+                                </div>
+                                <div class="card-body card-post-body">
+                                    <!-- Título del post -->
+                                    <h5 class="card-title card-post-title">{{ $post->title }}</h5>
+                                    <!-- Imagen del post -->
+                                    @if($post->imagen)
+                                    <img src="{{ asset('storage/' . $post->imagen) }}" class="img-fluid mb-3" style="max-height: 150px; object-fit: cover;" alt="Imagen del post">
+                                    @endif
+                                    <!-- Contenido del post -->
+                                    <p class="card-text card-post-text">{{ $post->content }}</p>
+                                    <!-- Botones de Editar y Eliminar -->
+                                    <div class="d-flex justify-content-end mt-3">
+                                        <a href="{{ route('post.edit', $post->id) }}" class="btn btn-warning btn-sm me-2">Editar</a>
+                                        <button type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#deleteModal" data-post-id="{{ $post->id }}">Eliminar</button>
+                                    </div>
+                                </div>
                             </div>
+                        </a>
                         </div>
                     </div>
-                </a>
+                </div>
                 @endforeach
             @endif
         </div>
--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,9 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
    Route::get('/profile', [ProfileController::class, 'postByUser'])->name('profile.index');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::post('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
